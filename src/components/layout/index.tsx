fd9904e05c70002b8332846cb7f268292e89eafe
Prawira Genestonlia <prawira.96@gmail.com>
2021-04-11 16:07:25 +0800

    fix: layout width

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,11 +8,13 @@ interface IComponentProps extends HTMLProps<HTMLDivElement> {
 
 const Component = (props: IComponentProps) => {
   return (
-    <div className={`w-screen h-screen text-xl font-body 
+    <div className="w-full h-full">
+      <div className={`w-full h-full text-xl font-body 
           text-black bg-white dark:text-white dark:bg-customNavy 
           ${props.padding && 'p-8 '}
           ${props.flex && 'flex flex-col flex-grow '}`}>
-      {props.fade ? <Fade cascade triggerOnce>{props.children}</Fade> : <>{props.children}</>}
+        {props.fade ? <Fade cascade triggerOnce>{props.children}</Fade> : <>{props.children}</>}
+      </div>
     </div>
   )
 }
